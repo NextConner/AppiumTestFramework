af8766547c097e7b95081274f96f3ccd1631ddc3
NextConner <m13576046405@163.com>
2018-01-10 18:56:39 +0800

    完成个人信息修改
    1.完成对单项个人信息的修改

--- IOSliveTest/src/pageObjects/UserPage.java
+++ IOSliveTest/src/pageObjects/UserPage.java
@@ -1,8 +1,10 @@
 package pageObjects;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 
 import io.appium.java_client.MobileElement;
@@ -12,28 +14,50 @@ import location.LocUserPage;
 public class UserPage {
 
 	public IOSDriver<MobileElement> driver;
-	//public AppiumDriver driver;
-	
+	// public AppiumDriver driver;
+
 	public UserPage(IOSDriver<MobileElement> driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
 
-	 @FindBy(xpath = LocUserPage.SETTINGXP) 
+	@FindBy(xpath = LocUserPage.SETTINGXP)
 	public WebElement setting;
 
-	@FindBys(value = { @FindBy(id = LocUserPage.INFO_EDITID), @FindBy(xpath = LocUserPage.INFO_EDITXP) })
+	@FindBy(id = LocUserPage.INFO_EDITID)
 	public WebElement infoEdit;
-	
-	@FindBy(className=LocUserPage.USER_NAME_CLASS)
+	@FindBy(xpath = LocUserPage.EDIT_USERNAME)
+	public WebElement editUserName;
+	@FindBy(xpath = LocUserPage.EDIT_SEX)
+	public WebElement editSex;
+	@FindBy(xpath = LocUserPage.EDIT_BIRTHDAY)
+	public WebElement editBirthday;
+	@FindBy(xpath=LocUserPage.BIRTHDAY)
+	public WebElement birthday;
+	@FindAll({ @FindBy(className = LocUserPage.BIRTHDAY_PICK_CALSS) })
+	public List<WebElement> birthList;
+	@FindBy(id=LocUserPage.DONE_ID)
+	public WebElement done;
+	@FindBy(id = LocUserPage.SAVE_INFOID)
+	public WebElement saveInfo;
+	@FindBy(className = LocUserPage.USER_NAME_CLASS)
 	public WebElement userName;
-	@FindBy(id=LocUserPage.COINS_ID)
+	@FindBy(id = LocUserPage.COINS_ID)
 	public WebElement coins;
-	@FindBy(id=LocUserPage.DIAMONDS_ID)
+	@FindBy(id = LocUserPage.DIAMONDS_ID)
 	public WebElement diamonds;
-	@FindBy(id=LocUserPage.LEVEL_ID)
+	@FindBy(id = LocUserPage.LEVEL_ID)
 	public WebElement level;
-	@FindBy(id=LocUserPage.BROADCAST_LEVEL_ID)
+	@FindBy(id = LocUserPage.BROADCAST_LEVEL_ID)
 	public WebElement broadcastLevel;
-	
+	@FindBy(id = LocUserPage.BACK_ID)
+	public WebElement infoBack;
+	@FindBy(id = LocUserPage.SEX_SECRETID)
+	public WebElement secret;
+	@FindBy(id = LocUserPage.SEX_FEMALEID)
+	public WebElement female;
+	@FindBy(id = LocUserPage.SEX_MALEID)
+	public WebElement male;
+	@FindBy(xpath = LocUserPage.NOW_SEX)
+	public WebElement nowSex;
 }
--- IOSliveTest/src/testCases/UserInfoTestCase.java
+++ IOSliveTest/src/testCases/UserInfoTestCase.java
@@ -1,16 +1,20 @@
 package testCases;
 
+import static org.testng.Assert.assertEquals;
+
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import initAppium.InitADriver;
@@ -49,10 +53,12 @@ public class UserInfoTestCase {
 	@BeforeMethod
 	public void setUp() throws Exception {
 		log.info("-----------------------------START!-----------------------------");
+
 		// TODO 在每条测试用例执行之前保证测试环境的一致性,建议使用reset 设置
-		if (count == 0) {
-			common.isAlert(driver);
-		}
+		// if (count == 0) {
+		// common.isAlert(driver);
+		// }
+
 		winWidth = driver.manage().window().getSize().getWidth();
 		winHeight = driver.manage().window().getSize().getHeight();
 		// 判断是否已经登录
@@ -66,18 +72,108 @@ public class UserInfoTestCase {
 
 	}
 
+	@Ignore
 	@Test
 	public void getUserInfo() {
 		log.info("当前登录用户 ： " + userPage.userName.getAttribute("name"));
-		log.info("用户金币数 ： "+userPage.coins.findElement(By.xpath("")).getAttribute("value"));
-		log.info("用户钻石数 ： "+userPage.diamonds.getText().substring(7));
-		log.info("用户等级 ： "+userPage.level.getText().substring(4));
-		log.info("用户等级 ： "+userPage.broadcastLevel.getText().substring(14));
+		// log.info("用户金币数 ：
+		// "+userPage.coins.findElement(By.xpath("")).getAttribute("name"));
+		// log.info("用户钻石数 ： "+userPage.diamonds.getText().substring(7));
+		// log.info("用户等级 ： "+userPage.level.getText().substring(4));
+		// log.info("用户等级 ： "+userPage.broadcastLevel.getText().substring(14));
+		userPage.infoEdit.click();
+	}
+
+	@Ignore
+	// @Parameters({"testName"}) String testName
+	@Test
+	public void editUserName() throws InterruptedException {
+		String userName = userPage.userName.getAttribute("name").trim();
+		log.info("当前登录用户 ： " + userName);
+
+		userPage.infoEdit.click();
+		userPage.editUserName.clear();
+		userPage.editUserName.sendKeys("UserName");
+		userPage.editUserName.click();
+		userPage.saveInfo.click();
+		log.info("保存");
+		assertEquals(userPage.userName.getAttribute("name"), "testName");
+
+	}
+
+	@Ignore
+	@Test
+	public void editUserSex() {
+		String[] sexs = { "Secret", "Female", "Male" };
+		int i = 0;
+		userPage.infoEdit.click();
+		userPage.editSex.click();
+		String nowSex = userPage.nowSex.getAttribute("value").trim();
+		switch (nowSex) {
+		case "Secret":
+			i = 1;
+			userPage.female.click();
+			break;
+		case "Male":
+			i = 0;
+			userPage.secret.click();
+			break;
+		case "Female":
+			i = 2;
+			userPage.male.click();
+			break;
+		default:
+			log.info("未获取到当前性别 ： " + nowSex);
+			break;
+		}
+		userPage.saveInfo.click();
+		log.info("保存");
+		userPage.infoEdit.click();
+		assertEquals(userPage.nowSex.getAttribute("value").trim(), sexs[i]);
+
+	}
+
+	// @Ignore
+	// int i
+	@Test
+	public void editUserBirth() throws InterruptedException {
+		int startX = 0;
+		int startY = 0;
+		int height = 0;
+		int i = 1;
+		userPage.infoEdit.click();
+		String nowBirthday = userPage.birthday.getAttribute("value").split("-")[0].trim();
+		log.info(nowBirthday);
+		userPage.editBirthday.click();
+		if (userPage.birthList.size() > 0) {
+			Point point = userPage.birthList.get(2).getLocation();
+			height = userPage.birthList.get(2).getSize().getHeight();
+			startX = point.x;
+			startY = point.y + (height / 2) - 25;
+		} else {
+			log.info("未获取到选择日期的UI!");
+		}
+		log.info(startX + " : " + startY + " : ");
+		for (;i<3; i++) {
+			driver.tap(1, startX, startY, 500);
+		}
+		userPage.done.click();
+		assertEquals(userPage.birthday.getAttribute("value").split("-")[0].trim(), String.valueOf(2018 - i).trim());
+		userPage.saveInfo.click();
+		log.info("保存");
+		count++;
 	}
 
 	@AfterMethod
 	public void tearDown(Method method) throws InterruptedException {
+
 		TimeUnit.SECONDS.sleep(5);
+	}
+
+	@AfterClass
+	public void destory() {
+		log.info("uninstall app");
+		// driver.removeApp("com.gomo.ios.gLive");
 		driver.quit();
 	}
 }
